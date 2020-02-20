e615c47c7f3d9cc69b030b126e0b7f40f9459801
apgomez <paul.gomezehmig@tcs.com>
2020-02-20 13:25:27 -0500

    Strain tested reverse function concatenation

--- strain/Strain.fs
+++ strain/Strain.fs
@@ -2,10 +2,13 @@
 
 let keep pred items = seq {
     for item in items do
+    // TODO: put the for contents within a separate function when code gets larger within do expression
         if pred item then
             yield item
 }
 
-let negate pred = not << pred
+let negate pred = pred >> not
 
-let discard pred items = keep (negate pred ) items
+let negate2 pred = not << pred // tested reverse function composition
+
+let discard pred items = keep (negate pred) items
