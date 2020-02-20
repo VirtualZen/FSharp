dced942b396f5c887c01bace6c6cc853442fdfce
apgomez <paul.gomezehmig@tcs.com>
2020-02-19 22:08:52 -0500

    upload

---
+++ strain/Strain.fs
@@ -0,0 +1,11 @@
+﻿module Seq
+
+let keep pred items = seq {
+    for item in items do
+        if pred item then
+            yield item
+}
+
+let negate pred = not << pred
+
+let discard pred items = keep (negate pred ) items
