ef450735b18f0f23093e953f7dfcc8160eddfde6
apgomez <paul.gomezehmig@tcs.com>
2020-02-25 13:47:46 -0500

    Version with Seq.filter

--- strain/Strain.fs
+++ strain/Strain.fs
@@ -1,13 +1,17 @@
 ﻿module Seq
 
-let keep pred items = seq {
+//easy way, almost cheating
+let keep pred items = Seq.filter pred items
+
+// manual loop, not so functional
+let keep2 pred items = seq {
     for item in items do
     // TODO: put the for contents within a separate function when code gets larger within do expression
         if pred item then
             yield item
 }
 
-let negate pred = pred >> not
+let negate pred = pred >> not // tested forward function composition
 
 let negate2 pred = not << pred // tested reverse function composition
 
