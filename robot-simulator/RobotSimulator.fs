c61c425cd32c4e684fa332bef1b33cb973749910
apgomez <paul.gomezehmig@tcs.com>
2020-02-25 23:26:36 -0500

    Changes inspired from excercise review

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -3,29 +3,28 @@
 type Direction = North | East | South | West
 type Position = int * int
 type Robot = { direction: Direction; position: Position }
+type Rotation = Left | Right
 
-// not obvious constructor call, it is possible to write robot with ...
 let create direction position = { direction = direction; position = position}
 
 let rotate robot newDirection =
     {robot with direction = newDirection}
 
-// would like to refactor moveRight and moveLeft with a single function
-// to use as parameter a list of tuples [(North,East);(East,South);(South,West);(West,North);]
-let moveRight robot = 
-    match robot.direction with
-        | North -> rotate robot East
-        | East -> rotate robot South
-        | South -> rotate robot West
-        | West -> rotate robot North
-
-let moveLeft robot = 
-    match robot.direction with
-        | North -> rotate robot West
-        | West -> rotate robot South
-        | South -> rotate robot East
-        | East -> rotate robot North
-        
+let turn rotation robot =
+    match rotation with
+        |Right ->
+            match robot.direction with
+                | North -> rotate robot East
+                | East -> rotate robot South
+                | South -> rotate robot West
+                | West -> rotate robot North
+        |Left ->
+            match robot.direction with
+                | North -> rotate robot West
+                | West -> rotate robot South
+                | South -> rotate robot East
+                | East -> rotate robot North
+    
 let walk robot =
     let (x, y) = robot.position
     match robot.direction with
@@ -36,18 +35,17 @@ let walk robot =
 
 let moveInstruction robot instruction =
     match instruction with
-        | 'R' -> moveRight robot
-        | 'L' -> moveLeft robot
+        | 'R' -> turn Right robot
+        | 'L' -> turn Left robot
         | 'A' -> walk robot
         | _ -> failwith "Wrong Instruction"
 
-let rec move instructions robot =
+let rec move2 instructions robot =
     let robot = Seq.head(instructions) |> moveInstruction robot
-    // TODO make tail work nicer, understand solution with fold
     let remainingInstructions = Seq.tail(instructions)
     if Seq.length remainingInstructions > 0 then
-        move remainingInstructions robot
+        move2 remainingInstructions robot
     else
         robot
 
-        
+let move instructions robot = Seq.fold (fun robot -> moveInstruction robot) robot instructions
--- strain/Strain.fs
+++ strain/Strain.fs
@@ -1,18 +1,26 @@
 ﻿module Seq
 
-//easy way, almost cheating
-let keep pred items = Seq.filter pred items
-
 // manual loop, not so functional
-let keep2 pred items = seq {
+let keep pred items = seq {
     for item in items do
     // TODO: put the for contents within a separate function when code gets larger within do expression
         if pred item then
             yield item
 }
 
+//easy way, cheating as excercise forbids using built in features
+let keep2 pred items = Seq.filter pred items
+
+//LINQ
+let keep3 pred items = query {
+    for item in seq items do
+        where (pred item)
+        select item
+}
+
 let negate pred = pred >> not // tested forward function composition
 
+//not good practice
 let negate2 pred = not << pred // tested reverse function composition
 
 let discard pred items = keep (negate pred) items
