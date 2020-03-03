836702ca716abaea278519efdbb1b4e8e6dca574
apgomez <paul.gomezehmig@tcs.com>
2020-03-02 22:04:44 -0500

    Recognizes 0 1

--- ocr-numbers/OcrNumbers.fs
+++ ocr-numbers/OcrNumbers.fs
@@ -1,5 +1,20 @@
 module OcrNumbers
+let zero =
+    [ " _ ";
+      "| |";
+      "|_|";
+      "   " ]
+
+let one =
+    [ "   ";
+      "  |";
+      "  |";
+      "   " ]
+
+let numbers = [zero;one]
 
 let convert input =
-    "0"
+    if input = zero then "0"
+    elif input = one then "1"
+    else ""
     |> Some
