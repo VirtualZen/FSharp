d6e022c4624e59fa30e4e418652c153d8683be3f
apgomez <paul.gomezehmig@tcs.com>
2020-03-02 22:41:05 -0500

    0-9

--- ocr-numbers/OcrNumbers.fs
+++ ocr-numbers/OcrNumbers.fs
@@ -1,20 +1,70 @@
 module OcrNumbers
 let zero =
-    [ " _ ";
-      "| |";
-      "|_|";
-      "   " ]
+    ([ " _ ";
+    "| |";
+    "|_|";
+    "   " ] , "0")
 
 let one =
-    [ "   ";
-      "  |";
-      "  |";
-      "   " ]
+    ([ "   ";
+    "  |";
+    "  |";
+    "   " ] , "1")
 
-let numbers = [zero;one]
+let two =
+    ([ " _ ";
+    " _|";
+    "|_ ";
+    "   " ] , "2")
+
+let three =
+    ([ " _ ";
+    " _|";
+    " _|";
+    "   " ] , "3")
+let four =
+    ([ "   ";
+    "|_|";
+    "  |";
+    "   " ] , "4")
+
+let five =
+    ([ " _ ";
+    "|_ ";
+    " _|";
+    "   " ] , "5")
+
+let six =
+    ([ " _ ";
+    "|_ ";
+    "|_|";
+    "   " ] , "6")
+
+let seven =
+    ([ " _ ";
+    "  |";
+    "  |";
+    "   " ] , "7")
+
+let eight =
+    ([ " _ ";
+    "|_|";
+    "|_|";
+    "   " ] , "8")
+let nine =
+    ([ " _ ";
+    "|_|";
+    " _|";
+    "   " ] , "9")
+
+let numbers = [zero ; one; two; three; four; five; six; seven; eight; nine]
+
+let table =
+    Map.ofList numbers
 
 let convert input =
-    if input = zero then "0"
-    elif input = one then "1"
-    else ""
+    let found = table.TryFind input
+    match found with
+    | Some x -> x
+    | None -> failwith "not found"
     |> Some
