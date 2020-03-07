99b31fed59761c1c813b8696dfc5251840eb4ee9
apgomez <paul.gomezehmig@tcs.com>
2020-03-06 23:14:43 -0500

    Single bottles working - deduplicated version

--- beer-song/BeerSong.fs
+++ beer-song/BeerSong.fs
@@ -1,3 +1,26 @@
 ﻿module BeerSong
+let getRemainingBottlesOnWall n =
+    match n with
+    | 1 ->  sprintf "%d bottle of beer on the wall." n
+    | 0 -> "no more bottles of beer on the wall."
+    | -1 -> "99 bottles of beer on the wall."
+    | _ -> sprintf "%d bottles of beer on the wall." n
 
-let recite (startBottles: int) (takeDown: int) = failwith "You need to implement this function."
+let getStartingBottles n =
+    match n with
+    | 1 -> "1 bottle of beer on the wall, 1 bottle of beer."
+    | 0 -> "No more bottles of beer on the wall, no more bottles of beer."
+    | _ -> sprintf "%d bottles of beer on the wall, %d bottles of beer." n n
+let getTakeBottleDown n =
+    match n with
+    | 1 -> "Take it down and pass it around, "
+    | 0 -> "Go to the store and buy some more, "
+    | _ -> "Take one down and pass it around, "
+
+// Deduplicated code, but harder to understand getStartingBottles getTakeBottleDown getRemainingBottlesOnWall
+let getVerse (n: int) =
+    [ getStartingBottles n;
+     getTakeBottleDown n + getRemainingBottlesOnWall (n-1) ]  
+
+let recite (startBottles: int) (takeDown: int) =
+    getVerse startBottles
