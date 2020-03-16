c584e2d48132e1ad14152af14e97a29687edc715
apgomez <paul.gomezehmig@tcs.com>
2020-03-16 00:43:51 -0500

    Single node

---
+++ binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,16 @@
+﻿module BinarySearchTree
+
+type node = { value : int; left: node option; right: node option}
+
+let left node  = node.left
+
+let right node = node.right
+
+let data node = node.value
+
+let createItem item = {value = item; left = None ; right = None}
+    
+let rec create items =
+    createItem (Seq.head items) //:: create (Seq.tail items)
+
+let sortedData node = failwith "You need to implement this function."
